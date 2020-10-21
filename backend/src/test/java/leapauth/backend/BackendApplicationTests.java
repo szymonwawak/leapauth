a40f2ba1831df439774c2e4e386767c7900bb7fe
Szymon <wawakszymon@gmail.com>
2020-10-21 21:20:26 +0200

    Initial commit

---
+++ backend/src/test/java/leapauth/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package leapauth.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
