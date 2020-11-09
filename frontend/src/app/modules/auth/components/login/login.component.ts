f6b75ef9d00a7023169a1aa45f4f421ea46765a9
Szymon <wawakszymon@gmail.com>
2020-11-09 21:18:06 +0100

    Added route navigation for auth template

--- frontend/src/app/modules/auth/components/login/login.component.ts
+++ frontend/src/app/modules/auth/components/login/login.component.ts
@@ -1,19 +1,21 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthApiService} from '../../../../core/services/auth-api.service';
 import {LoginModel} from '../../../../shared/models/LoginModel';
+import {NavigableBase} from '../../../../core/classes/navigable-base';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent extends NavigableBase implements OnInit {
   loginModel: LoginModel = {
     email: '',
     password: ''
   };
 
   constructor(private authService: AuthApiService) {
+    super();
   }
 
   ngOnInit(): void {
@@ -29,5 +31,17 @@ export class LoginComponent implements OnInit {
       },
       err => {
       });
+  }
+
+  down(): void {
+  }
+
+  left(): void {
+  }
+
+  right(): void {
+  }
+
+  top(): void {
   }
 }
